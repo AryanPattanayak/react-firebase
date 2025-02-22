28dbae25d8dd275602ffc74d62fd7bae62cf72a3
AryanPattanayak <myworkaryan@gmail.com>
2025-02-22 12:09:02 +0530

    added styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Root from "./Root";
+import "./style.css"
 createRoot(document.querySelector("#root")).render(<Root/>)
